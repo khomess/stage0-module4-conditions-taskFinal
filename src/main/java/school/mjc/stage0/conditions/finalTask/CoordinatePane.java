3eea84b7121633b9c67b4441261e87a85f1a1b44
khomess <sasha.khomenko12@gmail.com>
2022-11-20 10:21:18 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+
         if(x>0 && y>0){
             System.out.println("first");
         } else if(x<0 && y>0){
