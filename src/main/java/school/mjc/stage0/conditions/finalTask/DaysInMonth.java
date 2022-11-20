7e8ab69a90f79bf31c17c94deaac33f2921d80b2
khomess <sasha.khomenko12@gmail.com>
2022-11-20 10:18:26 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,56 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+            switch (month){
+                case(1):
+                case(3):
+                case(5):
+                case(7):
+                case(8):
+                case(10):
+                case(12):
+                    System.out.println("31");
+                    break;
+                case(4):
+                case(6):
+                case(9):
+                case(11):
+                    System.out.println("30");
+                    break;
+                case(2):
+                    System.out.println("29");
+                    break;
+                default:
+                    System.out.println("invalid date");
+                    break;
+            }
+        } else if (year>0){
+            switch (month){
+                case(1):
+                case(3):
+                case(5):
+                case(7):
+                case(8):
+                case(10):
+                case(12):
+                    System.out.println("31");
+                    break;
+                case(4):
+                case(6):
+                case(9):
+                case(11):
+                    System.out.println("30");
+                    break;
+                case(2):
+                    System.out.println("28");
+                    break;
+                default:
+                    System.out.println("invalid date");
+                    break;
+            }
+        } else{
+            System.out.println("invalid date");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int dividerEnd1 = 0;
+        if(divider !=0){
+            dividerEnd1 = dividend/divider;
+            int dividerEnd2 = dividerEnd1 * divider;
+            if (dividerEnd2 == dividend) {
+                System.out.println("can be divided completely");
+            } else {
+                System.out.println("cannot be divided completely");
+            }
+        } else {
+            System.out.println("division by zero");
+        }
     }
 }
